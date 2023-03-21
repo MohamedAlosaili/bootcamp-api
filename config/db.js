53025272d2c9e413bc5b5f68c043a37e837dbeb2
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-03-21 06:57:22 +0300

    Update style DB connection log with colors package

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const colors = require("colors");
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {
@@ -8,7 +9,9 @@ const connectDB = async () => {
     useFindAndModify: false,
   });
 
-  console.log(`MongoDB connected ✅ \n ${conn.connection.host}`);
+  console.log(
+    `MongoDB connected ${conn.connection.host} ✅`.underline.cyan.bold
+  );
 };
 
 module.exports = connectDB;
