e39a0b49f2a8ec0cc883c41e4e09f52f241e09ed
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-03-21 06:58:41 +0300

    Update add express body parser middleware & utilize the colors package

--- server.js
+++ server.js
@@ -2,19 +2,22 @@
 require("dotenv").config({ path: "./config/config.env" });
 
 const express = require("express");
-
-// Middlewares
 const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // Bootcamps route file
 const bootcamps = require("./routes/bootcamps");
 
 // Connect to Database
-const connectDB = require("./config/db");
 connectDB();
 
 // Initialize new express app
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Dev middleware logger
 if (process.env.NODE_ENV === "development") {
@@ -28,14 +31,14 @@ const PORT = process.env.PORT || 5000;
 
 // Run the app on .env.PORT or 5000 port as default port
 const server = app.listen(PORT, () => {
-  console.log(`Server running on PORT: ${PORT}`);
-  console.log(`Environment mode: ${process.env.NODE_ENV}`);
+  console.log(`Server running on PORT: ${PORT}`.yellow.bold);
+  console.log(`Environment mode: ${process.env.NODE_ENV}`.blue.bold);
 });
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (error, promise) => {
   // Print the error message
-  console.log(error?.toString());
+  console.log(`${error && error.toString()} ❌`.underline.red);
 
   // Close the server & exit process
   server.close(() => process.exit(1));
