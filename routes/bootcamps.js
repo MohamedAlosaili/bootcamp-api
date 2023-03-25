d9de694ce6948796deaf5f9f79c10b04bd3df4fd
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-03-25 14:17:47 +0300

    Add advancedResults middleware to getBootcamps route and add uploadBootcampPhoto route

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -7,20 +7,28 @@ const {
   updateBootcamp,
   deleteBootcamp,
   getBootcampsInRadius,
+  uploadBootcampPhoto,
 } = require("../controllers/bootcamps");
+
+const advancedResults = require("../middleware/advancedResults");
+const Bootcamp = require("../models/Bootcamp");
 
 const router = express.Router();
 
 // Re-route into other resource routers
 router.use("/:bootcampId/courses", courseRouter);
 
-router.route("/").get(getBootcamps).post(createBootcamp);
+router
+  .route("/")
+  .get(advancedResults(Bootcamp, "courses"), getBootcamps)
+  .post(createBootcamp);
 
 router
   .route("/:id")
   .get(getBootcamp)
   .put(updateBootcamp)
   .delete(deleteBootcamp);
+router.route("/:id/photo").put(uploadBootcampPhoto);
 
 router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
