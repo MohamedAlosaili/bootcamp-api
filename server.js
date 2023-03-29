dfdb15d024c2cbdfb4303d5204c12b0a31816afa
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-03-29 16:53:17 +0300

    Add users route to the server.js file

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const errorHandler = require("./middleware/error");
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
 const auth = require("./routes/auth");
+const users = require("./routes/users");
 
 // Connect to Database
 connectDB();
@@ -44,6 +45,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
 app.use("/api/v1/auth", auth);
+app.use("/api/v1/users", users);
 
 // Error Handler middleware
 app.use(errorHandler);
