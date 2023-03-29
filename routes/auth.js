773a6d4f8230c19957a304ed27368a2063e820a1
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-03-29 16:54:03 +0300

    Chore arrange required statements

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const router = express.Router();
+
 const {
   register,
   login,
@@ -11,8 +13,6 @@ const {
 } = require("../controllers/auth");
 
 const { protect } = require("../middleware/auth");
-
-const router = express.Router();
 
 router.post("/register", register);
 
