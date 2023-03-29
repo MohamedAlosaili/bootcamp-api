cd1e153437cab3b98f473de2f68c70bc502c3c0d
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-03-29 14:46:15 +0300

    Add routes to update user details and password

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
+  updatePassword,
 } = require("../controllers/auth");
 
 const { protect } = require("../middleware/auth");
@@ -17,6 +19,10 @@ router.post("/register", register);
 router.post("/login", login);
 
 router.get("/me", protect, getMe);
+
+router.put("/updatedetails", protect, updateDetails);
+
+router.put("/updatepassword", protect, updatePassword);
 
 router.post("/forgotpassword", forgotPassword);
 
