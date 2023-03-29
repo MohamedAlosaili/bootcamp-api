b1ca8139bdfe2708b5fb21c1afeeb5f231e8b789
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-03-29 12:13:34 +0300

    Add endpoints to handle forgotten and reset password requests and a protected endpoint to retrieve user information in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,25 @@
 const express = require("express");
-const { register, login } = require("../controllers/auth");
+
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/auth");
+
+const { protect } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.post("/register", register);
 
 router.post("/login", login);
+
+router.get("/me", protect, getMe);
+
+router.post("/forgotpassword", forgotPassword);
+
+router.put("/resetpassword/:resetToken", resetPassword);
 
 module.exports = router;
