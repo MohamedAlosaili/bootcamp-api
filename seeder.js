691977675bae8904e3f7ef4fb486a25d5edf2db2
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-03-30 22:13:16 +0300

    Add Review model and update seeder script

--- seeder.js
+++ seeder.js
@@ -19,18 +19,23 @@ mongoose.connect(process.env.MONGO_URI, {
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
 const User = require("./models/User");
+const Review = require("./models/Review");
 
-// Load bootcamps
+// Load files
 const bootcamps = JSON.parse(
   fs.readFileSync(path.join(__dirname, "_data", "bootcamps.json"), "utf-8")
 );
-// Load courses
+
 const courses = JSON.parse(
   fs.readFileSync(path.join(__dirname, "_data", "courses.json"), "utf-8")
 );
-// Load users
+
 const users = JSON.parse(
   fs.readFileSync(path.join(__dirname, "_data", "users.json"), "utf-8")
+);
+
+const reviews = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "_data", "reviews.json"), "utf-8")
 );
 
 if (process.argv[2] === "-i" || process.argv[2] === "--import") {
@@ -55,6 +60,7 @@ async function importDocumentsIntoDB() {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
 
     console.log("Documents loaded success ✅".green.inverse);
     process.exit();
@@ -70,6 +76,7 @@ async function deleteAllDocumentsFromDB() {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
 
     console.log("Documents Deleting success ✅".red.inverse);
     process.exit();
